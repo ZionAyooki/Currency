cdacf0f532f8e15812bebf718b97236ce02f424e
Zion Ayooki <zion.ayooki@gmail.com>
2021-02-06 04:17:41 +0200

    Setting up the boilerplate, added images

---
+++ src/app/CurrencyApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CurrencyApp extends React.Component {
+  render() {
+    return (
+      <div>This is my app</div>
+    );
+  }
+}
+
+export default CurrencyApp;
